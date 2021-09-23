5b96c974c52d7add458081a8bddb4cc17dfd914a
Rangel <bruna.henrique.rangel@gmail.com>
2021-09-22 22:01:42 -0300

    Adding assets folder and image folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import styled from "styled-components";
 import './index.css'
 
+import buttonAdd from "./assets/images/add.svg";
+import bin from "./assets/images/men-bin.svg";
+
 const App = () => {
 
   return (
